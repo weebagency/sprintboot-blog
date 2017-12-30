8457f22eed67783e46c32b38507e788a291d3630
cedrico <maruejolcedric@gmail.com>
2017-12-29 19:24:32 -0500

    added a few tests

--- src/main/java/blog/Application.java
+++ src/main/java/blog/Application.java
@@ -1,18 +1,17 @@
 package blog;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @RestController
 public class Application {
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
